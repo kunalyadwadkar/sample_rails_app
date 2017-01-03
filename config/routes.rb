93f7a26018c35acc296afb62b937ae5deb22f29b
Kunal Yadwadkar <kunal.y@udacity.com>
2017-01-03 16:02:40 -0500

    Created a user display page with a basic sign-on.

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
   
   get '/contact', to:'static_pages#contact'
 
+  resources :users
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#home'
 end
