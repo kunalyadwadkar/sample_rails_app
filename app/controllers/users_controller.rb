c7b463d009c8e3fc2d43b9bd0685944eecd5a36b
Kunal Yadwadkar <kunal.y@udacity.com>
2017-01-03 18:45:55 -0500

    Finished with Sign-up
    Added signature

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,15 +9,25 @@ class UsersController < ApplicationController
   end
 
   def create
+
     @user = User.new(user_params)
-    @user.save
 
-    render 'show'
+    if @user.save
+      flash[:success] = "Welcome to the Sample App"
+      redirect_to @user
+
+    else
+
+      render 'new'
+
+    end
+
   end
 
+  private
 
-  def user_params
-    params.require(:user).permit(:name, :email, :password,
-                                 :password_confirmation)
-  end
+   def user_params
+     params.require(:user).permit(:name, :email, :password,
+                                  :password_confirmation)
+   end
 end
